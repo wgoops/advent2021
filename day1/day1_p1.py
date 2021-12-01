6352f7f90f1976fd9668b1b4b62a5ce8bfc431a0
squigley <shaine.quigley@gmail.com>
2021-12-01 13:45:37 -0500

    day 1 p1 finished

---
+++ day1/day1_p1.py
@@ -0,0 +1,19 @@
+
+# open file to get data
+with open("data.txt") as f: 
+    data = f.read().splitlines()
+
+data_indexes = range(len(data))
+# convert data to list of integers
+for i in data_indexes: 
+    data[i] = int(data[i])
+
+count = 0
+# compare previous item with current 
+for j in data_indexes[1::]:
+    current = data[j]
+    previous = data[j-1]
+    if current > previous:
+        count += 1
+
+print(count)
