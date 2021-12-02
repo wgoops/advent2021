0a118872ef5988a431f3f7cffdfa66ffe9308f6c
squigley <shaine.quigley@gmail.com>
2021-12-02 11:25:13 -0500

    day2 p2

---
+++ day2/day2_p2.py
@@ -0,0 +1,26 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+with open("data.txt") as f: 
+    data = f.read().splitlines()
+
+data_dict = {}
+x, y, aim = 0, 0, 0
+for item in data: 
+    split_item = item.split(" ")
+    direction = split_item[0]
+    magnitude = int(split_item[1])
+
+    if direction == "forward":
+        x += magnitude
+        y += aim*magnitude
+    
+    if direction == "up": 
+        aim -= magnitude
+    
+    if direction == "down": 
+        aim += magnitude
+logging.info(f"x: {x}")
+logging.info(f"y: {y}")
+logging.info(f"aim: {aim}")
+logging.info(f"x*y: {x*y}")
