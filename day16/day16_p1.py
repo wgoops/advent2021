79d7658c36b6ace6697b386608e2b450aac60063
wgoops <wgoops@gmail.com>
2021-12-16 21:26:08 -0500

    literal packets function implemented

--- day16/day16_p1.py
+++ day16/day16_p1.py
@@ -15,11 +15,25 @@ def getHeaders(packet):
 
 def literal_packet(packet):
     packet = packet[6::]
-    
     #### binary_parser ####
     
-    bin_list = [num[i:i+5] for i in range(len(packet) / 5)
-    for num in bin_list: 
+    #### group the packet into groups of five ####
+    indexes = range(0, len(packet) - 1, 5)  # we can disregard the extra bits at the end 
+    print(indexes)
+    bin_list = []
+    for i in range(len(indexes) - 1): 
+        j_start = indexes[i]
+        j_end = indexes[i+1]
+        bin_list.append(packet[j_start:j_end])
+        if packet[j_start] == 0: 
+            break
+    full_num = ""
+    for item in bin_list: 
+        full_num = full_num + item[1::]
+    
+    return bin2int(full_num)
+
+
 def test_hex2bin():
     assert hex2bin("1AB") == "110101011"
     
@@ -32,3 +46,6 @@ def test_getHeaders():
     packet = "1AB"
     assert getHeaders(packet) == (6, 5)
 
+def test_literal_packet(): 
+    packet = "110100101111111000101000"   #this packet is from the example
+    assert literal_packet(packet) == 2021 
